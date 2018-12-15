e59543fe1f0fc49265f9f97c62965b8541423820
Stefan Wawrzyn <swawrzyn@gmail.com>
2018-12-15 17:24:10 +0800

    init for express server

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+// process json data easily
+const bodyParser = require('body-parser');
+// used for clients to access server
+const cors = require('cors');
+// for printing out logs
+const morgan = require('morgan');
+
+// building express server
+const app = express();
+
+// enabling packages
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.listen(process.env.PORT || 8081);
+
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Hello world!!',
+  });
+});
